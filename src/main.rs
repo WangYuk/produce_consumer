94fe1f6ed3acdc0b4de980ddb406cb38089ea37a
yukang.wang <yukang.wang@smartx.com>
2024-02-21 10:26:21 +0800

    mpsc model using tokio

--- src/main.rs
+++ src/main.rs
@@ -1,52 +1,21 @@
-use std::sync::{mpsc, Arc, Mutex};
-use std::thread;
+use tokio::sync::mpsc;
 
-
-fn producer(id: usize, tx: mpsc::Sender<(usize, usize)>) -> thread::JoinHandle<()> {
-    thread::spawn(move || {
-        for i in (id - 1) * 100 + 1..id * 100 {
-            tx.send((id, i)).unwrap();
-        }
-    })
-}
-
-fn consumer(
-    id: usize,
-    shared_rx: Arc<Mutex<mpsc::Receiver<(usize, usize)>>>,
-) -> thread::JoinHandle<()> {
-    thread::spawn(move || loop {
-        let rx = shared_rx.lock().unwrap();
-        if let Ok((i, n)) = rx.recv() {
-            println!("consumer {} get {} from producer {}", id, n, i);
-        } else {
-            return ();
-        }
-    })
-}
-
-fn main() {
-    let mut producer_v = vec![];
-    let mut consumer_v = vec![];
-    let (tx, rx) = mpsc::channel();
-    let shared_rx = Arc::new(Mutex::new(rx));
-
-    for i in 1..4 {
-        let hdl = consumer(i, shared_rx.clone());
-        consumer_v.push(hdl);
-    }
-
+#[tokio::main]
+async fn main() {
+    let (tx, mut rx) = mpsc::channel(32);
+    
     for i in 1..3 {
-        let hdl = producer(i, tx.clone());
-        producer_v.push(hdl);
+        let new_tx = tx.clone();
+        tokio::spawn(async move {
+            for j in (i - 1) * 100 + 1..i * 100 {
+                new_tx.send((i, j)).await.ok();
+            }
+        });
     }
 
     drop(tx);
 
-    for hdl in producer_v {
-        hdl.join().unwrap();
-    }
-
-    for hdl in consumer_v {
-        hdl.join().unwrap();
+    while let Some((id,message)) = rx.recv().await {
+        println!("consumer get {} from producer {}", message, id);
     }
 }
